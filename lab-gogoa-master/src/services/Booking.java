67d300e0d5ccb7fa7d8aad9962130b1a518b7a42
sanjivani05 <61271086+sanjivani05@users.noreply.github.com>
2020-04-04 21:54:00 +0530

    Add files via upload

---
+++ lab-gogoa-master/src/services/Booking.java
@@ -0,0 +1,18 @@
+package services;
+
+// Type your code
+
+//parent class
+public class Booking{
+	
+	//method returning rates
+	public double booking(int noOfPersons,int rates)
+	{
+	
+        double d=(double)noOfPersons*rates;
+        return d;
+		
+}
+	
+	
+}
